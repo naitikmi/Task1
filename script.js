fbac3dc362b0ef669a7cb36f08d2f58f0be61266
naitik <naitikm947@gmail.com>
2024-04-13 16:58:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+document.getElementById("show").addEventListener("click", function() {
+    document.getElementById("createform1").style.display = "block";
+});
+
+document.getElementById("close-form").addEventListener("click", function(){
+    document.getElementById("createform1").style.display = "none";
+    document.getElementById("hide-for-sign-in1").style.display = "block";
+    document.getElementById("hide-for-sign-in2").style.display = "block";
+    document.getElementById("update").textContent === "Create Account"  
+    document.getElementById("update1").textContent === "Create Account";
+
+});
+
+
+document.getElementById("sign-in").addEventListener("click", function() {
+    if(document.getElementById("hide-for-sign-in1").style.display = "block"){
+        document.getElementById("hide-for-sign-in1").style.display = "none";
+    } else{
+        document.getElementById("hide-for-sign-in1").style.display = "block";
+    }
+
+    if(document.getElementById("hide-for-sign-in2").style.display = "block"){
+        document.getElementById("hide-for-sign-in2").style.display = "none";
+    } else{
+        document.getElementById("hide-for-sign-in2").style.display = "block";
+    }
+
+    if(document.getElementById("update").textContent = "Create Account"){
+        document.getElementById("update").textContent = "Sign In";
+    }
+    else{
+        document.getElementById("update").textContent = "Create Account"  
+    }
+    if(document.getElementById("update1").textContent ="Create Account"){
+        document.getElementById("update1").textContent = "Sign In";
+    } else {
+        document.getElementById("update1").textContent = "Create Account";
+    }
+});
+
+document.getElementById("joingroup").addEventListener("click",function(){
+    document.getElementById("joingroup").style.display="none";
+    document.getElementById("leavegroup").style.display="inline";
+    document.getElementById("listgroupdiv").style.display="inline";
+})
+
+document.getElementById("leavegroup").addEventListener("click",function(){
+    document.getElementById("leavegroup").style.display="none";
+    document.getElementById("joingroup").style.display="inline";
+    document.getElementById("listgroupdiv").style.display="none";
+})
+
